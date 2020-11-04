5cde23de4b2a63024426f7c71610e3c441d2a4c8
JorgeArancibia1 <jorge.arancibia.jeal@gmail.com>
2020-11-04 01:14:17 +0000

    Deploying to gh-pages from  @ 75bcb8816bef77ebf2359d98fbecf552d7f4e066 🚀

---
+++ precache-manifest.ec4dad252b8e0d11b872e68b70be6a03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97691618594fc2c9f03e06c63c2658f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af201fc3ed28519e6037",
+    "url": "/static/css/main.36705d4a.chunk.css"
+  },
+  {
+    "revision": "063bab6028ab5e5bfd46",
+    "url": "/static/js/2.c06b3ae8.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/static/js/2.c06b3ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af201fc3ed28519e6037",
+    "url": "/static/js/main.6c92363f.chunk.js"
+  },
+  {
+    "revision": "8723e86cf2f7acb1c58f",
+    "url": "/static/js/runtime-main.f5db95df.js"
+  }
+]);
