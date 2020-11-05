1b9dbcc2baddc605c9572b5301662bc4b356fe74
koke <jorge.arancibia.jeal@gmail.com>
2020-11-05 17:06:54 -0300

    before changes

---
+++ src/screens/PostsScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navbar } from '../ui/Navbar';
+
+export const PostsScreen = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>PostsScreen</h1>
+    </div>
+  )
+}
