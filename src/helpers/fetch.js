a9552a093c25ba85b47a999f9cf4bdc7dbd9918b
koke <jorge.arancibia.jeal@gmail.com>
2020-11-08 19:35:46 -0300

    optimize2

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -5,15 +5,14 @@ const fetchSinToken = (endpoint, data, method = 'GET') => {
 
 	if (method === 'GET') {
 		return fetch(url);
-	} else {
-		return fetch(url, {
-			method,
-			headers: {
-				'Content-type': 'application/json',
-			},
-			body: JSON.stringify(data),
-		});
 	}
+	return fetch(url, {
+		method,
+		headers: {
+			'Content-type': 'application/json',
+		},
+		body: JSON.stringify(data),
+	});
 };
 
 const fetchConToken = (endpoint, data, method = 'GET') => {
